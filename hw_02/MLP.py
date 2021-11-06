3161128bf39687d029fb9993c93d0b31978e5f80
jballesteros <jballesteros@ablehumanmotion.com>
2021-11-06 11:22:11 +0100

    Add main.py to project with training and visualization tasks.
    Refactor Worksheet file to Dataset.
    Refactor variable names for a constant naming convention (PEP 8).
    Other simplifications.

--- hw_02/MLP.py
+++ hw_02/MLP.py
@@ -4,20 +4,21 @@ from Perceptron import Perceptron
 
 # ----------- task 4 "Multi-Layer Perceptron" ----------
 class MLP:
-    """Multi-Layer Perceptron instance.
+    """
+    Multi-Layer Perceptron instance.
 
     Specific for classifying inputs for logic gates.
     """
     def __init__(self):
-        self.hiddenLayer = []
+        self.hidden_layer = []
         # only 4 neurons needed in the hidden layer
         self._ps_in_hl: int = 4
         for _ in range(self._ps_in_hl):
             # two inputs per neuron
             perceptron = Perceptron(2)
-            self.hiddenLayer.append(perceptron)
+            self.hidden_layer.append(perceptron)
         # one output neuron, with 4 inputs coming from hidden layer.
-        self.outputPerceptron = Perceptron(self._ps_in_hl)
+        self.output_perceptron = Perceptron(self._ps_in_hl)
         # output value
         self.output = 0
 
@@ -28,25 +29,25 @@ class MLP:
 
         # activation of the hidden layer
         for i in range(self._ps_in_hl):
-            new_inputs[i] = self.hiddenLayer[i].forward_step(inputs)
+            new_inputs[i] = self.hidden_layer[i].forward_step(inputs)
 
         # activation of the output layer
-        self.output = self.outputPerceptron.forward_step(new_inputs)
+        self.output = self.output_perceptron.forward_step(new_inputs)
 
     def backprop_step(self, target: float) -> None:
         """Update parameters (delta and weights) on network."""
 
         # update output layer
         deltaN = -(target - self.output) * self.output * (1 - self.output)
-        self.outputPerceptron.update(deltaN)
+        self.output_perceptron.update(deltaN)
 
         # update error for the new layer
-        e = self.outputPerceptron.bias * deltaN
-        for i in range(len(self.outputPerceptron.weights)):
-            e += deltaN * self.outputPerceptron.weights[i]
+        e = self.output_perceptron.bias * deltaN
+        for i in range(len(self.output_perceptron.weights)):
+            e += deltaN * self.output_perceptron.weights[i]
 
         # update hidden layer perceptrons
-        for i in range(len(self.hiddenLayer)):
-            delta = e * self.outputPerceptron.inputs[i] * (1 - self.outputPerceptron.inputs[i])
-            self.hiddenLayer[i].update(delta)
+        for i in range(len(self.hidden_layer)):
+            delta = e * self.output_perceptron.inputs[i] * (1 - self.output_perceptron.inputs[i])
+            self.hidden_layer[i].update(delta)
 
