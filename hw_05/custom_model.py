2e461c4238cfd2eaf5f8636a723f3a8e28224fe5
jebape <jesusballesterosperal@gmail.com>
2021-11-26 21:43:23 +0100

    Remove custom layer class
    As stated in the last homework correction "It is perfectly fine and arguably more efficient to use the implementation by keras"

--- hw_05/custom_model.py
+++ hw_05/custom_model.py
@@ -3,58 +3,15 @@ import tensorflow as tf
 
 # ---------- task 2 "Model" -----------
 
-# Definition of a custom layer for our model.
-class CustomLayer(tf.keras.layers.Layer):
-    """A custom dense layer"""
-
-    def __init__(self, units: int, activation=tf.nn.sigmoid):
-        """Constructor
-
-        Args:
-          units: Input units.
-          activation: Activation function used in the forward pass.
-        """
-        super(CustomLayer, self).__init__()
-        self.units = units
-        self.activation = activation
-
-    def build(self, input_shape: tf.TensorShape) -> None:
-        """Instantiation of weights and bias
-
-        Args:
-          input_shape: shape for weights and bias creation.
-        """
-        self.w = self.add_weight(
-            shape=(input_shape[-1], self.units),
-            initializer="random_normal",
-            trainable=True,
-        )
-        self.b = self.add_weight(
-            shape=(self.units,),
-            initializer="random_normal",
-            trainable=True,
-        )
-
-    @tf.function
-    def call(self, inputs: tf.Tensor) -> tf.Tensor:
-        """Forward propagation
-
-        Args:
-           inputs: Inputs from layer.
-        """
-        incoming_inputs = tf.matmul(inputs, self.w) + self.b
-        return self.activation(incoming_inputs)
-
-
 class CustomModel(tf.keras.Model):
     """Custom Model with convolutional and pooling layers."""
 
     def __init__(self):
         """Constructor"""
         super(CustomModel, self).__init__()
-        self.hidden_layer1 = CustomLayer(units=64)
-        self.hidden_layer2 = CustomLayer(units=64)
-        self.output_layer = CustomLayer(units=1)
+        self.hidden_layer1 = tf.keras.layers.Dense(64)
+        self.hidden_layer2 = tf.keras.layers.Dense(64)
+        self.output_layer = tf.keras.layers.Dense(1)
 
     @tf.function
     def call(self, inputs: tf.Tensor) -> tf.Tensor:
@@ -96,7 +53,7 @@ class ConvModel(tf.keras.Model):
             strides=2, padding="same"
         )
 
-        self.output_layer = CustomLayer(units=10, activation=tf.nn.softmax)
+        self.output_layer = tf.keras.layers.Dense(10, activation=tf.nn.softmax)
 
     @tf.function
     def call(self, inputs: tf.Tensor) -> tf.Tensor:
